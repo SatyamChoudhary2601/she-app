02e8cc20d1fd59ae93a0df57edee6ad3e5d5dcd1
Satyamchoudhary2601 <satyamchoudhary2601@gmail.com>
2021-01-21 22:41:47 +0530

    neeww

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import MainBody from "./components/MainBody";
 import Welcome from "./components/Welcome";
 import Header from "./components/Header";
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Route exact path="/she-app" component={Welcome} />
+        <Route exact path="/" component={Welcome} />
         <Route path="/swipe" component={MainBody} />
         <Route path="/video" component={Video} />
         {/* <Route path="/card" component={CardFlip} /> */}
